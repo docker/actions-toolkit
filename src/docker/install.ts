9a7c8c8d01e2533ee8acf805600c5803bc289035
Paweł Gronowski <pawel.gronowski@docker.com>
2024-09-09 14:43:37 +0200

    docker/install: Handle missing `v` prefix when searching GH release
    Signed-off-by: Paweł Gronowski <pawel.gronowski@docker.com>

--- src/docker/install.ts
+++ src/docker/install.ts
@@ -587,7 +587,10 @@ EOF`,
     }
     const releases = <Record<string, GitHubRelease>>JSON.parse(body);
     if (!releases[version]) {
-      throw new Error(`Cannot find Docker release ${version} in ${url}`);
+      if (!releases['v' + version]) {
+        throw new Error(`Cannot find Docker release ${version} in ${url}`);
+      }
+      return releases['v' + version];
     }
     return releases[version];
   }
