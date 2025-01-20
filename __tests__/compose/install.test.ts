12299862529544e341567879f040c09eb26c0dec
CrazyMax <1951866+crazy-max@users.noreply.github.com>
2025-01-20 15:38:48 +0100

    compose: cloud releases support
    Signed-off-by: CrazyMax <1951866+crazy-max@users.noreply.github.com>

--- __tests__/compose/install.test.ts
+++ __tests__/compose/install.test.ts
@@ -98,34 +98,61 @@ describe('download', () => {
 });
 
 describe('getDownloadVersion', () => {
-  it('returns latest download version', async () => {
+  it('returns official latest download version', async () => {
     const version = await Install.getDownloadVersion('latest');
+    expect(version.key).toEqual('official');
     expect(version.version).toEqual('latest');
     expect(version.downloadURL).toEqual('https://github.com/docker/compose/releases/download/v%s/%s');
     expect(version.releasesURL).toEqual('https://raw.githubusercontent.com/docker/actions-toolkit/main/.github/compose-releases.json');
   });
-  it('returns v2.24.3 download version', async () => {
+  it('returns official v2.24.3 download version', async () => {
     const version = await Install.getDownloadVersion('v2.24.3');
+    expect(version.key).toEqual('official');
     expect(version.version).toEqual('v2.24.3');
     expect(version.downloadURL).toEqual('https://github.com/docker/compose/releases/download/v%s/%s');
     expect(version.releasesURL).toEqual('https://raw.githubusercontent.com/docker/actions-toolkit/main/.github/compose-releases.json');
   });
+  it('returns cloud latest download version', async () => {
+    const version = await Install.getDownloadVersion('cloud:latest');
+    expect(version.key).toEqual('cloud');
+    expect(version.version).toEqual('latest');
+    expect(version.downloadURL).toEqual('https://github.com/docker/compose-desktop/releases/download/v%s/%s');
+    expect(version.releasesURL).toEqual('https://raw.githubusercontent.com/docker/actions-toolkit/main/.github/compose-lab-releases.json');
+  });
+  it('returns cloud v2.27.1-desktop.1 download version', async () => {
+    const version = await Install.getDownloadVersion('cloud:v2.27.1-desktop.1');
+    expect(version.key).toEqual('cloud');
+    expect(version.version).toEqual('v2.27.1-desktop.1');
+    expect(version.downloadURL).toEqual('https://github.com/docker/compose-desktop/releases/download/v%s/%s');
+    expect(version.releasesURL).toEqual('https://raw.githubusercontent.com/docker/actions-toolkit/main/.github/compose-lab-releases.json');
+  });
+  it('unknown repo', async () => {
+    await expect(Install.getDownloadVersion('foo:bar')).rejects.toThrow(new Error('Cannot find compose version for foo:bar'));
+  });
 });
 
 describe('getRelease', () => {
-  it('returns latest GitHub release', async () => {
+  it('returns latest official GitHub release', async () => {
     const version = await Install.getDownloadVersion('latest');
     const release = await Install.getRelease(version);
     expect(release).not.toBeNull();
     expect(release?.tag_name).not.toEqual('');
   });
-  it('returns v2.24.3 GitHub release', async () => {
+  it('returns v2.24.3 official GitHub release', async () => {
     const version = await Install.getDownloadVersion('v2.24.3');
     const release = await Install.getRelease(version);
     expect(release).not.toBeNull();
     expect(release?.id).toEqual(138380726);
     expect(release?.tag_name).toEqual('v2.24.3');
     expect(release?.html_url).toEqual('https://github.com/docker/compose/releases/tag/v2.24.3');
+  });
+  it('returns v2.27.1-desktop.1 cloud GitHub release', async () => {
+    const version = await Install.getDownloadVersion('cloud:v2.27.1-desktop.1');
+    const release = await Install.getRelease(version);
+    expect(release).not.toBeNull();
+    expect(release?.id).toEqual(157591108);
+    expect(release?.tag_name).toEqual('v2.27.1-desktop.1');
+    expect(release?.html_url).toEqual('https://github.com/docker/compose-desktop/releases/tag/v2.27.1-desktop.1');
   });
   it('unknown release', async () => {
     const version = await Install.getDownloadVersion('foo');
