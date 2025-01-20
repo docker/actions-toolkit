d9bd2d45ba633e761e6ebb26b881832e5309fe0c
CrazyMax <1951866+crazy-max@users.noreply.github.com>
2025-01-20 15:40:14 +0100

    test: increase timeout for binary download
    Signed-off-by: CrazyMax <1951866+crazy-max@users.noreply.github.com>

--- __tests__/compose/install.test.ts
+++ __tests__/compose/install.test.ts
@@ -36,7 +36,7 @@ describe('download', () => {
     ['v2.32.4', true],
     ['latest', true]
   ])(
-  'acquires %p of compose (standalone: %p)', async (version, standalone) => {
+    'acquires %p of compose (standalone: %p)', async (version, standalone) => {
       const install = new Install({standalone: standalone});
       const toolPath = await install.download(version);
       expect(fs.existsSync(toolPath)).toBe(true);
@@ -57,22 +57,26 @@ describe('download', () => {
     ['v2.31.0'],
     ['v2.32.4'],
   ])(
-  'acquires %p of compose with cache', async (version) => {
-    const install = new Install({standalone: false});
-    const toolPath = await install.download(version);
-    expect(fs.existsSync(toolPath)).toBe(true);
-  });
+    'acquires %p of compose with cache', async (version) => {
+      const install = new Install({standalone: false});
+      const toolPath = await install.download(version);
+      expect(fs.existsSync(toolPath)).toBe(true);
+    },
+    100000
+  );
 
   // prettier-ignore
   test.each([
     ['v2.27.1'],
     ['v2.28.0'],
   ])(
-  'acquires %p of compose without cache', async (version) => {
-    const install = new Install({standalone: false});
-    const toolPath = await install.download(version, true);
-    expect(fs.existsSync(toolPath)).toBe(true);
-  });
+    'acquires %p of compose without cache', async (version) => {
+      const install = new Install({standalone: false});
+      const toolPath = await install.download(version, true);
+      expect(fs.existsSync(toolPath)).toBe(true);
+    },
+    100000
+  );
 
   // TODO: add tests for arm
   // prettier-ignore
@@ -86,7 +90,7 @@ describe('download', () => {
     ['linux', 'ppc64'],
     ['linux', 's390x'],
   ])(
-  'acquires compose for %s/%s', async (os, arch) => {
+    'acquires compose for %s/%s', async (os, arch) => {
       jest.spyOn(osm, 'platform').mockImplementation(() => os as NodeJS.Platform);
       jest.spyOn(osm, 'arch').mockImplementation(() => arch);
       const install = new Install();
